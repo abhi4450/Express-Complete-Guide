73a47faf54ccc547072d3bbd0f552f5aedec5a49
abhi4450 <abhishek.anshu1991>
2024-01-08 22:07:17 +0530

    fetched data from form

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("in the middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  console.log("in  middleware");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add</button></form>'
+  );
 });
-app.use((req, res, next) => {
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   console.log("in another middleware");
   res.send("<h1>Hello from Express</h1>");
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Listening to the port 3000.");
+});
