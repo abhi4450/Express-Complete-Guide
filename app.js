cf78ef8acb410f4c255b98a9a267c94da4374976
abhi4450 <abhishek.anshu1991>
2024-01-09 23:57:08 +0530

    served css and html

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -6,15 +7,16 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use("/admin", adminRoutes);
 
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+app.use("/", (req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000, () => {
-  console.log("Listening to the port 3000.");
+  console.log("server listening on port 3000.");
 });
